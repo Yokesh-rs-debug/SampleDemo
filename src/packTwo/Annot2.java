981ada404fd10d0018d4d895408c673b7b5203b2
admin <70yokesh@gmail.com>
2023-07-05 20:16:03 +0530

    Automation script commit

---
+++ src/packTwo/Annot2.java
@@ -0,0 +1,30 @@
+package packTwo;
+
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class Annot2 {
+
+	
+	@BeforeSuite
+	public void installsoftware()
+	{
+		System.out.println("I am the First");
+		System.out.println("I am the Second");
+		System.out.println("I am the Third");
+		System.out.println("I am the Fourth");
+	}
+	
+	@Test
+	public void software()
+	{
+		System.out.println("I am the Test");
+	}
+	@AfterSuite
+	public void deinstallsoftware()
+	{
+		System.out.println("I am the Last");
+	}
+	
+}
